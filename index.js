06271c785b7d33d6b151c2fc5d3a5d6b9306d236
Reldalk <Reldalk@gmail.com>
2018-09-27 17:36:06 -0400

    unfinished

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+
+'use strict';
 function generateResultHtml(addItemResult) {
   
-    return `
+  return `
 
     <li>
         <span class="shopping-item">${addItemResult}</span>
@@ -14,26 +16,38 @@ function generateResultHtml(addItemResult) {
         </div>
       </li>
     `;
-  }
+}
 
 
 
 function handleSubmit() {
-    $('form').submit(event => {
-        event.preventDefault();
-        const item = $('.js-shopping-list-entry').val();
-        $('.js-shopping-list-entry').val('');
-        console.log(item);
+  $('form').submit(event => {
+    event.preventDefault();
+    const item = $('.js-shopping-list-entry').val();
+    if(item !== ''){ 
+      $('.js-shopping-list-entry').val('');
+      console.log(item);
 
-        $('.shopping-list').append(generateResultHtml(item));
-    })
+      $('.shopping-list').append(generateResultHtml(item));
+    }
+  }
+  ); 
     
 }
 
+function checkButton() {
+  $('shopping-item-toggle').on('click' event => {
+    event.preventDefault();
+    $(this).toggleClass('shopping-item__checked');
+  })
+}
+/*
+function 
 name of the class and add a new class to the class list
 remove the css class
 
 look for this.closest li.remove 
 
+//.html(results.map(generateResultHtml)--*/
 $(handleSubmit);
-//.html(results.map(generateResultHtml)
+$(checkButton);
---
+++ old/index.js
@@ -0,0 +1,9 @@
+'use strict';
+$(function() {
+  $('.hero,large-image').click(function(event){
+    event.stopPropagation(); 
+    console.log(event);
+  });
+}    
+);
+
